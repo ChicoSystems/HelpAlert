444d0986072dd5477faf6f8a2002c98aa9be21ec
Isaac Assegai Travers <isaac.a.travers@gmail.com>
2020-04-04 11:28:35 -0700

    adding helpRequest_processor.php, wiring up backend
    Signed-off-by: Isaac Assegai Travers <isaac.a.travers@gmail.com>

---
+++ app/helpRequest_processor.php
@@ -0,0 +1,16 @@
+<?php
+/* Isaac Travers
+   Hackaton 2020
+   Help Alert
+   helpRequest_processor.php
+
+   This will process the form submission from helpRequest.html
+   1. Figure out who user is from $_SESSION variable.
+   2. get help request text
+   3. Get location Data
+   4. Query db for list of users to send request to
+   5. Notify those users
+
+
+
+?>
