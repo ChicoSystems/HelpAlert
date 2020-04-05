ba6454d93f4cd72b947d420ebbf23023d5ce0b7b
Robert Airth <rja45@humboldt.edu>
2020-04-05 11:23:41 -0700

    fixed top cutoff issue with login.html
    Signed-off-by: Robert Airth <rja45@humboldt.edu>

--- app/helpFound.php
+++ app/helpFound.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 
-if(array_key_exists("userid", $_SESSION){
-    //user is not logged in, redirect to login
-	header("Location: login.php");
+if(array_key_exists("userid", $_SESSION)){
+    /*user is not logged in, redirect to login*/
+    header("Location: login.php");
 
 }else{
 	$_SESSION["message"] = "Help! I've fallen and I can't get up!";
@@ -17,17 +17,38 @@ if(array_key_exists("userid", $_SESSION){
 
 <!--
     by: Robert Airth
-    Modified By: Isaac Travers & Robert Airth
-    last modified: 4/4/2020
+	Modified by: Isaac Travers and Robert Airth
+    last modified: 4/5/2020
 -->
 
 <head>
-    <title>HelpAlert Request Summary</title>
+    <title>HelpAlert Login</title>
     <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="https://unpkg.com/purecss@1.0.1/build/pure-min.css" integrity="sha384-" crossorigin="anonymous">
+    
+
+    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.1/build/grids-responsive-min.css">
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+    <link rel="stylesheet" href="css/marketing.css">
 </head>
 
-<body>
+<body class="background centered-content">
 
+<div class="header">
+    <div class="home-menu pure-menu pure-menu-horizontal pure-menu-fixed">
+        <a class="pure-menu-heading" href="">Help Alert!</a>
+
+        <ul class="pure-menu-list">
+            <li class="pure-menu-item pure-menu-selected"><a href="#" class="pure-menu-link">Home</a></li>
+            <li class="pure-menu-item"><a href="#" class="pure-menu-link">Profile</a></li>
+            <li class="pure-menu-item"><a href="#" class="pure-menu-link">Settings</a></li>
+        </ul>
+    </div>
+</div>
+
+<div class="background centered-content-wide">
+    <div class=" centered-content-wide">
     <h2> Your request for help has been answered! </h2>
     <form method="get"
           action="helpFound_processor.php">
@@ -57,6 +78,7 @@ if(array_key_exists("userid", $_SESSION){
         <p> Only click Finished once you are finished being helped. </p>
         <input type="submit" value="Finished" name="finished" />
     </form>
+</div>
 
 </body>
 </html>
