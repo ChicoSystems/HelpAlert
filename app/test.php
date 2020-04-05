18484524d94d7db0e1cdf56efbf03a8839389337
Isaac Assegai Travers <isaac.a.travers@gmail.com>
2020-04-04 18:38:11 -0700

    got db connection to work in test.php
    Signed-off-by: Isaac Assegai Travers <isaac.a.travers@gmail.com>

--- app/test.php
+++ app/test.php
@@ -7,10 +7,35 @@ if(array_key_exists("userid", $_SESSION)){
 require_once("hsu_conn_helpAlert.php");
 require_once("cfg/db_options.php");
 
-echo "hello".$_SESSION["dbPass"]." ".$_SESSION["dbUser"];
+//echo "hello".$_SESSION["dbPass"]." ".$_SESSION["dbUser"];
+$conn = hsu_conn_helpAlert($_SESSION["dbUser"], $_SESSION["dbPass"]);
+
+if($conn){
+  echo "connection succedded";
+
+  //test query
+  $query_string = "select * from appuser";
+
+  $empl_stmt = oci_parse($conn, $query_string);
+
+  oci_execute($empl_stmt, OCI_DEFAULT);
+
+  while (oci_fetch($empl_stmt)){
+    $userid = oci_result($empl_stmt, "USER_ID");
+    $lat = oci_result($empl_stmt, "LATITUDE");
+    $long = oci_result($empl_stmt, "LONGITUDE");
+    echo "user_id: ".$userid." ".$lat." ".$long;
+  }
 
 }else{
+  echo "connection failed";
+}
 
+print_r($conn, true);
+
+
+}else{
+	header("Location login.php");
 }
 
 
