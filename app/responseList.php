957f00d3bffcff21bf66faca9721031e1826abc8
rja45 <54596406+rja45@users.noreply.github.com>
2020-04-04 18:45:35 -0700

    Added css files and img elements and fixed responseList.php
    Added css files and img elements and fixed responseList.php

--- app/responseList.php
+++ app/responseList.php
@@ -2,69 +2,87 @@
 
 session_start();
 if(!array_key_exists("userid", $_SESSION)){
-	//not logged in
-	header("Location: login.php");
+        //not logged in
+        header("Location: login.php");
 }
 else
 {
 
 //get info from db here
-//oci_select_statements	
-	
+//oci_select_statements
 
-	//assign variables from db to session
-	//assign info to session
-	$data = array();
-    $data.array_push(array("proximity" => "0.2 Miles",
+
+        //assign variables from db to session
+        //assign info to session
+    $data = array();
+
+    $tempy = array("proximity" => "0.2 Miles",
                    "username" => "ToddTheDude",
                    "karma" => "100%",
-                   "response" => "I'll be free to help in 20 min"));
-    $data.array_push(array("proximity":"0.3 Miles",
-                   "username":"LocoMoco420",
-                   "karma" : "98%",
-                   "response":"I can help later today."));
+                   "response" => "I'll be free to help in 20 min");                                $
+    array_push($data, $tempy);
 
-    $data.array_push(array("proximity"=>"0.9 Miles",
+    $tempy = array("proximity"=>"0.3 Miles",
+                    "username"=>"LocoMoco420",
+                    "karma" => "98%",
+                    "response"=>"I can help later today.");
+                    array_push($data, $tempy);
+
+    $tempy = array("proximity"=>"0.9 Miles",
                    "username"=>"TigerKing39",
                    "karma" => "10%",
-                   "response"=>"I'll help you if you have a tiger."));
+                   "response"=>"I'll help you if you have a tiger.");
+                   array_push($data, $tempy);
 
-    $data.array_push(array("proximity"=>"1.2 Miles",
+    $tempy = array("proximity"=>"0.9 Miles",
+                   "username"=>"TigerKing39",
+                   "karma" => "10%",
+                   "response"=>"I'll help you if you have a tiger.");
+                   array_push($data, $tempy);
+
+    $tempy = array("proximity"=>"1.2 Miles",
                    "username"=>"KorbenDallas100",
                    "karma" => "100%",
-                   "response"=>"Negative, I am a meat popsicle."));
+                   "response"=>"Negative, I am a meat popsicle.");
+                   array_push($data, $tempy);
 
-    $data.array_push(array("proximity"=>"1.3 Miles",
+    $tempy = array("proximity"=>"1.3 Miles",
                    "username"=>"CheckTheDeck22",
                    "karma" => "90%",
-                   "response"=>"I'm free after 2pm. "));
+                   "response"=>"I'm free after 2pm. ");
+                   array_push($data, $tempy);
 
-    $data.array_push(array("proximity"=>"1.7 Miles",
+                   $tempy = array("proximity"=>"1.7 Miles",
                    "username"=>"NismoBoy19",
                    "karma" => "100%",
-                   "response"=>"I've never done that before but I'll give it a shot."));
+                   "response"=>"I've never done that before but I'll give it a shot.");
+                   array_push($data, $tempy);
 
-    $data.array_push(array("proximity"=>"1.9 Miles",
+    $tempy = array("proximity"=>"1.9 Miles",
                    "username"=>"WakeyBakey707",
                    "karma" => "0%",
-                   "response"=>"Trade labor for 215? "));
+                   "response"=>"Trade labor for 215? ");
+                   array_push($data, $tempy);
 
-    $data.array_push(array("proximity"=>"2.0 Miles",
+    $tempy = array("proximity"=>"2.0 Miles",
                    "username"=>"626Drift",
                    "karma" => "100%",
-                   "response"=>"I am not very good at that, but I'm willing to help you do it."));
+                   "response"=>"I am not very good at that, but I'm willing to help you do it.");
+                   array_push($data, $tempy);
 
-    $data.array_push(array("proximity"=>"2.1 Miles",
+    $tempy = array("proximity"=>"2.1 Miles",
                    "username"=>"SickManji69",
                    "karma" => "100%",
-                   "response"=>"I'm really good at that."));
+                   "response"=>"I'm really good at that.");
+                   array_push($data, $tempy);
 
-    $data.array_push(array("proximity"=>"2.4 Miles",
+    $tempy = array("proximity"=>"2.4 Miles",
                    "username"=>"ATFtrollLOL",
                    "karma" => "80%",
-                   "response"=>"I'm free after 4:30pm today."));
-
+                   "response"=>"I'm free after 4:30pm today.");
+                   array_push($data, $tempy);
 ?>
+
 
 
 <!DOCTYPE html>
@@ -75,7 +93,7 @@ else
     last modified: 4/4/2020
 
     you can run this using the URL:
-    
+
 
 -->
 
@@ -87,7 +105,7 @@ else
 <body>
     <form method="get"
           action="FILL IN PROPER PAGE HERE">
-        <h3>These people want to help you</h3>  
+        <h3>These people want to help you</h3>
         <table>
             <tr> <th scope="col"> Proximity </th>
                  <th scope="col"> Username </th>
@@ -95,19 +113,22 @@ else
                  <th scope="col"> Response </th>
                  <th scope="col"> Accept ? </th>
             </tr>
+
             <?php
+            $counter = 0;
             foreach($data as $temp1)
             {
-                             
+
                     ?>
                     <tr>
                     <td><?=$temp1["proximity"]?></td>
                     <td><?=$temp1["username"]?></td>
                     <td><?=$temp1["karma"]?></td>
                     <td><?=$temp1["response"]?></td>
+                    <td><input type="checkbox" name="SELECTEDUSERNAME"/></td>
                     </tr>
                     <?php
-                                
+
             }
             ?>
 
@@ -122,5 +143,3 @@ else
 <?php
 
 }//end of if-else
-
-?>
